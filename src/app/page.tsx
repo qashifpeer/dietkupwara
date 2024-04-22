5779313a65a4c0bf036b58954a371b158f5757ec
qashifpeer <mail.qashifpeer@gmail.com>
2024-04-22 23:32:39 +0530

    ft:nav added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="">
+    <section className="max-w-5xl mx-auto px-4">
       
-Welcome to  District Institute Of Education Kupwara
+<h1 className="text-center font-extrabold tangerine-bold text-4xl my-4">District Institute Of Education <br/>Kupwara</h1>
+<div className="flex justify-center items-center">
+  <Link href="/"> <button className="bg-sky-500 px-4 cursor-pointer py-2 rounded-full font-bold hover:bg-sky-300 duration-300">View Result</button>
+  </Link>
+  </div>
      
 
       
-    </main>
+    </section>
   );
 }
