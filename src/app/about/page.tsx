9d1ad36099d4129a78a3c6b53cc58ad1e2f6a204
qashifpeer <mail.qashifpeer@gmail.com>
2024-04-24 21:58:29 +0530

    result update

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div>page under construction..</div>
+  )
+}
+
+export default AboutUs
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContactUs = () => {
+  return (
+    <div>Designed and maintained by : Qashif Peer</div>
+  )
+}
+
+export default ContactUs
