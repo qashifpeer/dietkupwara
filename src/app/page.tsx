5ea01c61664f3992ad9bb0b9bec682153471b79e
qashifpeer <mail.qashifpeer@gmail.com>
2024-04-25 16:57:19 +0530

    link added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,19 +14,21 @@ export default function Home() {
         Class 8th Results 2024
       </h2>
       <div className="flex justify-center bg-green-200 z-50">
+        <Link href="https://dietkup.educian.com/campus_management_front_end_client/onlineresults">
         <button
           type="button"
           className="bg-sky-500 cursor-pointer hover:bg-red-400 w-80 h-20 border border-transparent rounded-md box-border p-4 m-4"
         >
           View Result
         </button>
+        </Link>
       </div>
 
       {/* <div className="bg-green-200 flex">
         <h2 className="my-6 text-lg text-slate-100 font-bold bg-slate-900 px-4 py-2">
           
         </h2>
-        <Link href="https://dietkup.educian.com/campus_management_front_end_client/onlineresults">
+        <Link href="">
           {" "}
           <button className="bg-sky-500 px-4 cursor-pointer py-2 rounded-full font-bold hover:bg-sky-300 duration-300 w-[300px]">
             View Result
