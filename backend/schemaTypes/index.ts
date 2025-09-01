3627c2d50142f15e49394f26b0785382ad5ea597
qashifpeer <mail.qashifpeer@gmail.com>
2025-09-02 00:45:42 +0530

    Remove sanity backend from repo

--- backend/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import employee from './employee'
-import level from './level'
-
-export const schemaTypes = [post, employee, category, level, blockContent]
